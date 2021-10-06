e62041039bcdf61dda3771eea1e4cd70ed80c76c
sinb-dev <sinb@techcollege.dk>
2021-10-06 11:37:02 +0200

    Added webapi and proved connection between api and spa

---
+++ Constellation WebApi/Controllers/ContainerController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Constellation_WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContainerController : ControllerBase
+    {
+        public class Response {
+            public string message {get;set;}
+        }
+        private readonly ILogger<ContainerController> _logger;
+
+        public ContainerController(ILogger<ContainerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Response Get(string myrequest="")
+        {
+            Response response = new();
+            if (myrequest != "")
+                response.message = myrequest;
+            else
+                response.message = "Du er bare SÅ dygtig!!!";
+            return response;
+        }
+    }
+}
--- pwa/src/components/Dashboard.vue
+++ pwa/src/components/Dashboard.vue
@@ -1,14 +1,19 @@
 <template>
   <h1>Dashboard - container</h1>
-  <input type="button" value="Don't click me" @click="sendRequest"/>
+  <input type="button" :value="label" @click="sendRequest"/>
+  <input type="text" v-model="param" placeholder="Write message to server" />
 </template>
 
 <script setup>
 import axios from 'axios'
+import { ref } from 'vue'
+var label = ref("CORS I RØVEN");
+var param = ref("");
+
 function sendRequest() 
 {
-  axios.get("https://jsonplaceholder.typicode.com/todos/1")
-    .then(response => console.log(response.data))
+  axios.get("http://localhost:5000/Container?myrequest="+param.value)
+    .then(response => label.value = response.data.message)
 }
 
 
