df8a392e15beba70f7e569d7d5f5325385243526
sinb-dev <sinb@techcollege.dk>
2021-10-06 10:29:36 +0200

    Added PWA project

---
+++ pwa/src/components/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Dashboard - container</h1>
+  <input type="button" value="Don't click me" @click="sendRequest"/>
+</template>
+
+<script setup>
+import axios from 'axios'
+function sendRequest() 
+{
+  axios.get("https://jsonplaceholder.typicode.com/todos/1")
+    .then(response => console.log(response.data))
+}
+
+
+</script>
+
+<style>
+
+</style>
