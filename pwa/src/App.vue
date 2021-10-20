c3273c368d126ea10338fbdbb643afa4527895e8
simonhoxer <simonhoxer@gmail.com>
2021-10-20 07:31:30 +0200

    pwa layout

--- pwa/src/App.vue
+++ pwa/src/App.vue
@@ -1,18 +1,39 @@
 <template>
-  <div id="nav" v-if="store.state.show_nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  
-  <router-view/>
+  <q-layout view="hHh lpR fFf" >
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar icon="view_in_ar" size="64px">
+            <!--<img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">-->
+            
+          </q-avatar>
+          Constellation
+        </q-toolbar-title>
+
+        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container class="fixed-center" style="padding-top:500px">
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
 </template>
+
 <script setup>
-//import { ref } from 'vue'
-import store from '@/store'
-//var show_nav = ref(store.state.show_nav)
-//console.log(show_nav.value)
+//import store from '@/store'
+import { ref } from 'vue'
+
+
+const rightDrawerOpen = ref(false)
+
+function toggleRightDrawer () {
+  rightDrawerOpen.value = !rightDrawerOpen.value
+}
 </script>
-
-<style lang="scss">
-
-</style>
--- pwa/src/views/Dashboard.vue
+++ pwa/src/views/Dashboard.vue
@@ -1,6 +1,17 @@
 <template>
+<q-page >
   <div class="q-pa-md q-gutter-md">
-    <q-list bordered padding class="rounded-borders" style="max-width: 350px">
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel magna a lorem bibendum viverra in et libero. Curabitur fringilla ullamcorper odio, sit amet gravida tortor ultricies at. Sed ac consectetur ante. Sed facilisis sem justo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur vitae dolor mauris. Fusce at lobortis magna. Curabitur ante leo, rhoncus et fringilla eget, tincidunt sit amet lacus. Quisque sodales ante nec orci volutpat, eu faucibus diam lobortis. Vivamus imperdiet diam et risus ornare, et mollis purus rutrum. Duis non tortor id neque ornare aliquet. Ut porta neque vel est laoreet, nec vulputate nulla congue. Nullam eu turpis nulla. Donec consequat est odio, posuere posuere est pellentesque in.</p>
+
+<p>Nam scelerisque maximus tellus, quis varius odio congue ut. Cras nunc felis, maximus vel maximus ut, convallis tristique enim. Praesent dapibus elit at dolor aliquam pharetra. Nam vehicula congue dolor non rutrum. Nullam facilisis, nisl quis condimentum facilisis, augue sem auctor eros, eu dignissim urna lorem sit amet velit. Vivamus tristique massa in ullamcorper rutrum. Etiam at justo et libero auctor fringilla ac vel est. Etiam volutpat nec libero non pulvinar. Praesent hendrerit consectetur neque, vel tempus risus suscipit quis. Suspendisse nec sem ac enim consectetur ullamcorper quis id purus. Cras pharetra tellus ac egestas pellentesque. Suspendisse tempus justo ut cursus malesuada. Curabitur tincidunt lectus nec lectus elementum, sit amet pulvinar orci dapibus.</p>
+
+<p>In semper, orci convallis vestibulum iaculis, leo dolor blandit metus, quis mattis est ante id nisi. Aenean vulputate sagittis leo, eget tincidunt nisi euismod vitae. Vestibulum non mollis libero. Duis a facilisis massa. Sed vel magna bibendum, placerat risus nec, commodo ante. Quisque a velit eu eros fermentum varius in non erat. Etiam viverra ipsum a sem luctus, non tincidunt tellus semper.</p>
+
+<p>Vestibulum pharetra accumsan dolor a auctor. Aenean ornare dapibus maximus. In hac habitasse platea dictumst. Cras rhoncus eget metus non hendrerit. Suspendisse faucibus augue lectus, sed pretium elit pharetra vitae. Nam faucibus magna quis molestie porttitor. In et nibh et nulla tristique blandit. Aliquam venenatis nibh nec ligula finibus, in ornare quam aliquam. Suspendisse vitae lorem ligula.</p>
+
+<p>Mauris et ipsum tincidunt, scelerisque quam ac, venenatis ipsum. In hac habitasse platea dictumst. Fusce vitae odio a urna ornare gravida. Maecenas ac auctor sapien. Donec et lacinia ligula. Mauris ut nisi efficitur, placerat purus a, vulputate nulla. Nullam sollicitudin ante molestie, accumsan nibh et, egestas diam. Curabitur dapibus laoreet lacus, eu tempor augue ornare eu. Quisque tempor accumsan ipsum, sed feugiat ipsum vestibulum sit amet. In hac habitasse platea dictumst. Nunc gravida suscipit ligula, sit amet facilisis elit aliquet id. Nulla purus massa, ornare sit amet metus aliquam, porta congue sapien. Nullam ullamcorper ac elit eget pulvinar. Cras eu lacus iaculis, rutrum lectus luctus, finibus magna. Pellentesque porta vitae ipsum ac vestibulum. Maecenas pellentesque maximus ipsum in maximus. </p>
+    <q-list bordered padding class="rounded-borders" >
       <q-item-label header>Folders</q-item-label>
 
       <q-item clickable v-ripple>
@@ -97,6 +108,7 @@
       </q-item>
     </q-list>
   </div>
+</q-page>
 </template>
 
 <script setup>
